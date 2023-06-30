628d8924229039bbfcc6b075b78b456036c4ca16
OmarIslam9 <109994919+OmarIslam9@users.noreply.github.com>
2023-06-30 17:21:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template,request
+
+app=Flask(__name__)
+
+data={}
+
+@app.route("/")
+def start():
+    return render_template("start.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        phone=request.form.get("phone")
+        password=request.form.get("password")
+        for key in data:
+            if phone==key:
+                if password==data[key]:
+                 return "Hello"
+
+        return "something went Wrong"
+
+    return render_template("login.html")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method=="POST":
+        phone=request.form.get("phone")
+        print(phone)
+        password=request.form.get("password")
+        print(password)
+        data[phone]=password
+        print(data)
+        return "registered"
+
+
+    return render_template("register.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__==("__main__"):
+    app.run(debug=True)
