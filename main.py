bfe90a09d270598a5ecb694a0bc3e624baacb86d
OmarIslam9 <109994919+OmarIslam9@users.noreply.github.com>
2023-06-30 17:40:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,12 @@
-from flask import Flask, render_template,request
-
-app=Flask(__name__)
-
-data={}
-
-@app.route("/")
-def start():
-    return render_template("start.html")
-
-@app.route("/login",methods=["GET","POST"])
-def login():
-    if request.method=="POST":
-        phone=request.form.get("phone")
-        password=request.form.get("password")
-        for key in data:
-            if phone==key:
-                if password==data[key]:
-                 return "Hello"
-
-        return "something went Wrong"
-
-    return render_template("login.html")
-
-@app.route("/register",methods=["GET","POST"])
-def register():
-    if request.method=="POST":
-        phone=request.form.get("phone")
-        print(phone)
-        password=request.form.get("password")
-        print(password)
-        data[phone]=password
-        print(data)
-        return "registered"
+from typing import List, Optional
 
 
-    return render_template("register.html")
+def main(args: Optional[List[str]] = None) -> int:
+    """This is preserved for old console scripts that may still be referencing
+    it.
 
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__==("__main__"):
-    app.run(debug=True)
+    return _wrapper(args)
